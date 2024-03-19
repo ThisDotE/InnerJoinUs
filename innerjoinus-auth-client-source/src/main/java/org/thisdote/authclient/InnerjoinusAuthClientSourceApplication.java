5129dc0214ef54c74016127f49150e75b02ca7cc
jhlee6515 <45275759+jhlee6515@users.noreply.github.com>
2024-03-20 00:07:49 +0900

    인증/인가 관련 클라이언트 생성

---
+++ innerjoinus-auth-client-source/src/main/java/org/thisdote/authclient/InnerjoinusAuthClientSourceApplication.java
@@ -0,0 +1,13 @@
+package org.thisdote.authclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnerjoinusAuthClientSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnerjoinusAuthClientSourceApplication.class, args);
+    }
+
+}
---
+++ innerjoinus-auth-client-source/src/test/java/org/thisdote/authclient/InnerjoinusAuthClientSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thisdote.authclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnerjoinusAuthClientSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
