04ae81826a4abc386aaa36fdc8f4f4aaa0d18e6f
shindaeyeong <eodud3196@gmail.com>
2024-03-27 09:56:07 +0900

    CORS 통신 완료

--- innerjoinus-article-reply-source/src/main/java/org/thisdote/innerjoinus/articlereply/configuration/CORSConfiguration.java
+++ innerjoinus-article-reply-source/src/main/java/org/thisdote/innerjoinus/articlereply/configuration/CORSConfiguration.java
@@ -12,6 +12,8 @@ public class CORSConfiguration implements WebMvcConfigurer {
 
             corsRegistry.addMapping("/**")
                     .exposedHeaders("Set-Cookie")
-                    .allowedOrigins("http://127.0.0.1");
+                    .allowedOrigins("http://localhost:5173") // 클라이언트 애플리케이션이 실행 중인 정확한 URL로 변경
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 필요한 메소드에 따라 조정
+                    .allowCredentials(true); // 필요한 경우 설정
     }
 }
